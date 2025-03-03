28f8e2061a9235102c0350af38a94dc456e347bb
sanil <sanildsouza960@gmail.com>
2025-03-03 12:06:18 +0530

    edited surveyroutes.js

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,6 +7,20 @@ const authMiddleware = require("../middleware/authMiddleware.js");
 router.post("/survey ",async (req,res)=>{
  try{
     const{name, gender, nationality, email, countryCode, phoneNumber, address, message}=req.body;
+    if (!name || !gender || !national || !email || !countryCode || !phoneNumber){
+      return res.status(400).json({message:"All fields are required"});
+    }
+    if(!email.match(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/)){
+      return res.status(400).json({message:"Invalid email"});
+    }
+    if(!phoneNumber.match(/^[0-9]{10}$/)){
+      return res.status(400).json({message:"Invalid phone number"});
+    }
+    const exisstinSurveyemail =await Survey.findOne({email});
+    const exisstinSurveyPhone =await Survey.findOne({phoneNumber});
+    if(exisstinSurveyemail || exisstinSurveyPhone){
+      return res.status(400).json({message:"Survey already exists"});
+    }
     const newSurvey = new Survey({
         name, 
         gender,
